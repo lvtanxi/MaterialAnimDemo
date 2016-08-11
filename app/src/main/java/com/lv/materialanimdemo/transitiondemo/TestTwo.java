d2d38814e921c81c089874df035c17ebb1edbece
lvyong <lvyong@retail-tek.com>
2016-08-11 15:02:40 +0800

    新增红包动画

--- app/src/main/java/com/lv/materialanimdemo/transitiondemo/TestTwo.java
+++ app/src/main/java/com/lv/materialanimdemo/transitiondemo/TestTwo.java
@@ -1,8 +1,15 @@
 package com.lv.materialanimdemo.transitiondemo;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.lv.materialanimdemo.R;
 
@@ -13,9 +20,97 @@ import com.lv.materialanimdemo.R;
  * Description:
  */
 public class TestTwo extends AppCompatActivity{
+
+    private FrameLayout flContainer;
+    private LinearLayout llyFront;
+    private LinearLayout llyBack;
+    private boolean isShowFront = true;
+
+    private AnimatorSet mFrontAnimator;
+    private AnimatorSet mBackAnimator;
+
+    private TextView tv;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_two);
+        setContentView(R.layout.content_main);
+        initView();
+        initAnimator();
+        setAnimatorListener();
+        setCameraDistance();
+    }
+
+    /**
+     * 初始化View
+     */
+    private void initView() {
+
+        flContainer = (FrameLayout)findViewById(R.id.fl_container);
+        llyFront = (LinearLayout) findViewById(R.id.lly_front);
+        llyBack = (LinearLayout) findViewById(R.id.lly_back);
+    }
+
+    /**
+     * 1.初始化动画
+     */
+    private void initAnimator() {
+        mFrontAnimator = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.anim_in);
+        mBackAnimator = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.anim_out);
+    }
+
+    /**
+     * 2.设置视角间距，防止旋转时超出边界区域
+     */
+
+    private void setCameraDistance() {
+        int distance = 6000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        llyFront.setCameraDistance(scale);
+        llyBack.setCameraDistance(scale);
+    }
+
+    /**
+     * 3.设置动画监听事件
+     */
+    private void setAnimatorListener() {
+
+        mFrontAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                flContainer.setClickable(false);
+            }
+        });
+
+        mBackAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                flContainer.setClickable(true);
+            }
+        });
+    }
+
+    /**
+     * 4.开启动画
+     * @param view
+     */
+    public void startAnimation(View view) {
+
+        //显示正面
+        if(!isShowFront) {
+            mFrontAnimator.setTarget(llyFront);
+            mBackAnimator.setTarget(llyBack);
+            mFrontAnimator.start();
+            mBackAnimator.start();
+            isShowFront = true;
+        } else {
+            mFrontAnimator.setTarget(llyBack);
+            mBackAnimator.setTarget(llyFront);
+            mFrontAnimator.start();
+            mBackAnimator.start();
+            isShowFront = false;
+        }
     }
 }
